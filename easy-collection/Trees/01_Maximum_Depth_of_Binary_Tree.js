f56a9d2aca1664f79c0df5151db1219b0d48c82c
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-03 15:36:39 +0300

    add iterative DFS solution for Trees 01

--- easy-collection/Trees/01_Maximum_Depth_of_Binary_Tree.js
+++ easy-collection/Trees/01_Maximum_Depth_of_Binary_Tree.js
@@ -35,8 +35,39 @@ function _maxDepth(root) {
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
 
+function __maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
+
+  const stack = [{ root, depth: 1 }];
+  let maxDepth = 1;
+
+  while (stack.length) {
+    const node = stack.pop();
+
+    if (!node.root) {
+      continue;
+    }
+
+    if (node.depth > maxDepth) {
+      maxDepth = node.depth;
+    }
+
+    if (node.root.left) {
+      stack.push({ root: node.root.left, depth: node.depth + 1 });
+    }
+
+    if (node.root.right) {
+      stack.push({ root: node.root.right, depth: node.depth + 1 });
+    }
+  }
+
+  return maxDepth;
+}
+
 const { TreeBuilder } = require('./00_Tree_Builder');
 
 const root = TreeBuilder.fromArray([3, 9, 20, null, null, 15, 7]);
 
-console.log(maxDepth(root));
+console.log(__maxDepth(root));
