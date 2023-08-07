385f2f774cf46ec5da8697dff26518abdf96a835
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-07 12:26:13 +0300

    add iterative solution for Trees 05

--- easy-collection/Trees/05_Convert_Sorted_Array_to_Binary_Search_Tree.js
+++ easy-collection/Trees/05_Convert_Sorted_Array_to_Binary_Search_Tree.js
@@ -17,7 +17,7 @@ function sortedArrayToBST(arr) {
       return null;
     }
 
-    const mid = Math.floor((i + j) / 2);
+    const mid = Math.trunc((i + j) / 2);
     const node = new TreeNode(arr[mid], fn(i, mid - 1), fn(mid + 1, j));
 
     return node;
@@ -35,12 +35,44 @@ function sortedArrayToBST(arr) {
     return null;
   }
 
-  const mid = Math.floor(arr.length / 2);
+  const mid = Math.trunc(arr.length / 2);
   const root = new TreeNode(
     arr[mid],
     sortedArrayToBST(arr.slice(0, mid)),
     sortedArrayToBST(arr.slice(mid + 1)),
   );
+
+  return root;
+}
+
+/**
+ * @param {number[]} arr
+ * @return {TreeNode}
+ */
+function sortedArrayToBST(arr) {
+  if (!arr.length) {
+    return null;
+  }
+
+  const root = new TreeNode();
+  const stack = [{ node: root, i: 0, j: arr.length - 1 }];
+
+  while (stack.length) {
+    const { node, i, j } = stack.pop();
+    const mid = Math.trunc((i + j) / 2);
+
+    node.val = arr[mid];
+
+    if (i < mid) {
+      node.left = new TreeNode();
+      stack.push({ node: node.left, i, j: mid - 1 });
+    }
+
+    if (j > mid) {
+      node.right = new TreeNode();
+      stack.push({ node: node.right, i: mid + 1, j });
+    }
+  }
 
   return root;
 }
