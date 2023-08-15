e2a69774c67a747dcbea3a2da08f6f8b08c773f7
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-15 17:45:35 +0300

    update Dynamic Programming 04 solution

--- easy-collection/Dynamic_Programming/04_House_Robber.js
+++ easy-collection/Dynamic_Programming/04_House_Robber.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 function rob(nums) {
-  if (nums.length === 0) {
-    return 0;
-  }
+  const sums = new Array(nums.length + 1);
 
-  const maxSums = new Array(nums.length + 1);
-
-  maxSums[0] = 0;
-  maxSums[1] = nums[0];
+  sums[0] = 0;
+  sums[1] = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    maxSums[i + 1] = Math.max(maxSums[i], maxSums[i - 1] + nums[i]);
+    const sumIfSkip = sums[i];
+    const sumIfRobe = sums[i - 1] + nums[i];
+
+    sums[i + 1] = Math.max(sumIfSkip, sumIfRobe);
   }
 
-  return maxSums[nums.length];
+  return sums[nums.length];
 }
 
 const nums = [2, 7, 9, 3, 1];
