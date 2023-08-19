60efc851e177bec5de38be5225b3c15c0f355fc5
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-19 14:40:00 +0300

    update Others 01 solution

--- Others/01_Number_of_1_Bits.js
+++ Others/01_Number_of_1_Bits.js
@@ -32,3 +32,32 @@ function _hammingWeight(n) {
 
   return k;
 }
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+function __hammingWeight(k) {
+  return k
+    .toString(2)
+    .split('')
+    .filter((ch) => ch === '1').length;
+}
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+function ___hammingWeight(n) {
+  const s = n.toString(2);
+
+  return s === '0' ? 0 : s.match(/1/g).length;
+}
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+function ____hammingWeight(n) {
+  return n.toString(2).replaceAll('0', '').length;
+}
