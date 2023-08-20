e10132348f7cba0e555fe111f3529ff5d69db3ed
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-20 23:21:28 +0300

    add Others 03 binary operations solution

--- Others/03_Reverse_Bits.js
+++ Others/03_Reverse_Bits.js
@@ -17,3 +17,24 @@ function reverseBits(n) {
 function _reverseBits(n) {
   return Number.parseInt(n.toString(2).split('').reverse().join('').padEnd(32, '0'), 2);
 }
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+function __reverseBits(n) {
+  let nRev = 0;
+
+  for (let i = 0; i < 32; i++) {
+    nRev <<= 1;
+    nRev += n & 1;
+
+    n >>>= 1;
+  }
+
+  return nRev >>> 0;
+}
+
+console.log(reverseBits(3221225471));
+console.log(_reverseBits(3221225471));
+console.log(__reverseBits(3221225471));
