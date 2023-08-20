d65ab8b43f3fd67686684339532ddf7518cfcefd
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-20 23:20:05 +0300

    add Others 01 XOR operator solution

--- Others/02_Hamming_Distance.js
+++ Others/02_Hamming_Distance.js
@@ -37,3 +37,37 @@ function _hammingDistance(x, y) {
 
   return k;
 }
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+function __hammingDistance(x, y) {
+  return (x ^ y).toString(2).replaceAll('0', '').length;
+}
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+function ___hammingDistance(x, y) {
+  let n = x ^ y;
+  let k = 0;
+
+  while (n !== 0) {
+    if ((n & 1) === 1) {
+      k++;
+    }
+
+    n >>>= 1;
+  }
+
+  return k;
+}
+
+console.log(hammingDistance(1, 3));
+console.log(_hammingDistance(1, 3));
+console.log(__hammingDistance(1, 3));
+console.log(___hammingDistance(1, 3));
