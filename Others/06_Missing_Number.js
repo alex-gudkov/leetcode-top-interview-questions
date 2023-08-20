4c5b793fe027bbf1ee97743248ae6f833bda53fb
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-20 23:24:29 +0300

    add Others 06 binary operations solution

--- Others/06_Missing_Number.js
+++ Others/06_Missing_Number.js
@@ -50,8 +50,40 @@ function __missingNumber(nums) {
   return l;
 }
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function ___missingNumber(nums) {
+  let res = nums.length;
+
+  for (let i = 0; i < nums.length; i++) {
+    res ^= i;
+    res ^= nums[i];
+  }
+
+  return res;
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function ____missingNumber(nums) {
+  const n = nums.length;
+  let res = ((n + 1) / 2) * n;
+
+  for (let i = 0; i < n; i++) {
+    res -= nums[i];
+  }
+
+  return res;
+}
+
 const nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 
 console.log(missingNumber(nums));
 console.log(_missingNumber(nums));
 console.log(__missingNumber(nums));
+console.log(___missingNumber(nums));
+console.log(____missingNumber(nums));
