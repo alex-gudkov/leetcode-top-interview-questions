d67f02053210699423e223d67926f372dcf99a40
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-20 23:18:15 +0300

    add new Others 01 binary operations solution && use console table

--- Others/01_Number_of_1_Bits.js
+++ Others/01_Number_of_1_Bits.js
@@ -61,3 +61,39 @@ function ___hammingWeight(n) {
 function ____hammingWeight(n) {
   return n.toString(2).replaceAll('0', '').length;
 }
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+function _____hammingWeight(n) {
+  let k = 0;
+
+  while (n !== 0) {
+    if ((n & 1) === 1) {
+      k++;
+    }
+
+    n >>>= 1;
+  }
+
+  return k;
+}
+
+const table = {};
+
+for (let i = 0; i <= 20; i++) {
+  const key = i.toString();
+
+  table[key] = {
+    bin: i.toString(2),
+    hw1: hammingWeight(i),
+    hw2: _hammingWeight(i),
+    hw3: __hammingWeight(i),
+    hw4: ___hammingWeight(i),
+    hw5: ____hammingWeight(i),
+    hw6: _____hammingWeight(i),
+  };
+}
+
+console.table(table);
