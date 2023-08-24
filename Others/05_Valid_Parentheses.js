d8d0b2cd073399a13ec2fb12e480c9dd1f86ef3e
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-24 14:53:00 +0300

    add new Others 05 solution

--- Others/05_Valid_Parentheses.js
+++ Others/05_Valid_Parentheses.js
@@ -23,6 +23,28 @@ function isValid(s) {
   return stack.length === 0;
 }
 
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function _isValid(s) {
+  const stack = [];
+
+  for (const ch of s) {
+    if (ch === '{' || ch === '[' || ch === '(') {
+      stack.push(ch);
+    } else {
+      const p = stack.pop();
+
+      if ((ch === '}' && p !== '{') || (ch === ']' && p !== '[') || (ch === ')') & (p !== '(')) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
 const s = '()[{]}';
 
 console.log(isValid(s));
+console.log(_isValid(s));
