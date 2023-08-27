f6a3b4636103722a5cfb6edcf8b230f143543de0
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-28 02:44:19 +0300

    add new Math 04 solution

--- easy-collection/Math/04_Roman_to_Integer.js
+++ easy-collection/Math/04_Roman_to_Integer.js
@@ -52,6 +52,27 @@ function _romanToInt(s) {
   return res + digits[s[s.length - 1]];
 }
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function __romanToInt(s) {
+  const digits = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  };
+  const nums = s.split('').map((ch) => digits[ch]);
+
+  return nums.reduce((res, num, i) => (num < nums[i + 1] ? res - num : res + num), 0);
+}
+
 const s = 'MCMXCIV';
 
 console.log(romanToInt(s));
+console.log(_romanToInt(s));
+console.log(__romanToInt(s));
