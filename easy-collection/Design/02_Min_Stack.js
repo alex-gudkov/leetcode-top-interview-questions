89591707b46a858c8e9e6dd3922e414160d514b6
alex-gudkov <gudkov.bsuir@gmail.com>
2023-08-30 09:52:56 +0300

    add new Design 02 solution

--- easy-collection/Design/02_Min_Stack.js
+++ easy-collection/Design/02_Min_Stack.js
@@ -38,6 +38,44 @@ MinStack.prototype.getMin = function () {
   return this.minStack[this.minStack.length - 1];
 };
 
+function _MinStack() {
+  this.stack = [];
+  this.n = 0;
+}
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+_MinStack.prototype.push = function (val) {
+  const minVal = this.n ? Math.min(val, this.stack[this.n - 1].minVal) : val;
+
+  this.stack[this.n] = { val, minVal };
+  this.n++;
+};
+
+/**
+ * @return {void}
+ */
+_MinStack.prototype.pop = function () {
+  this.n--;
+  this.stack.splice(this.n, 1);
+};
+
+/**
+ * @return {number}
+ */
+_MinStack.prototype.top = function () {
+  return this.stack[this.n - 1].val;
+};
+
+/**
+ * @return {number}
+ */
+_MinStack.prototype.getMin = function () {
+  return this.stack[this.n - 1].minVal;
+};
+
 /**
  * Your MinStack object will be instantiated and called as such:
  */
